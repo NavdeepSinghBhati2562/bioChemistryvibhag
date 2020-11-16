aa0aae826edc59d9273fe68cb32d6b0ffab96b1c
navdeep <navdeepsinghbhati2562@gmail.com>
2020-11-16 12:42:42 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:flutter/services.dart';
 void main() {
   runApp(MyApp());
 }
+//main
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
